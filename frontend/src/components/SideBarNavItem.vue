906feecad53c417dd975ae75fe95ad451436e373
Rebecca Bracho <rebeccabracho05@gmail.com>
2025-10-08 19:14:06 -0400

    fix import

--- frontend/src/components/SideBarNavItem.vue
+++ frontend/src/components/SideBarNavItem.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, defineEmits, computed, } from 'vue';
+import { computed, } from 'vue';
 defineOptions({ name: 'SideBarNavItem' });
 
 const props = defineProps({
