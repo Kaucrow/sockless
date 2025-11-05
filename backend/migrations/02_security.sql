089d641f42f6dd0a71606d3c7bd93ac0e283071c
kaucrow <kaucrow@protonmail.com>
2025-11-04 20:20:03 -0400

    feat: Added BO database tables & `todo.md`

--- backend/migrations/02_security.sql
+++ backend/migrations/02_security.sql
@@ -1,3 +1,4 @@
+--- Table: security.user
 CREATE TABLE IF NOT EXISTS security.user (
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email VARCHAR(50) UNIQUE NOT NULL,
@@ -6,37 +7,46 @@ CREATE TABLE IF NOT EXISTS security.user (
     surname VARCHAR(50) NOT NULL
 );
 
+--- Table: security.profile
 CREATE TABLE IF NOT EXISTS security.profile (
     profile_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "name" VARCHAR(50) UNIQUE NOT NULL
 );
 
+--- Table: security.subsystem
 CREATE TABLE IF NOT EXISTS security.subsystem (
     subsystem_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     "name" VARCHAR(50) UNIQUE NOT NULL
 );
 
+--- Table: security.class
 CREATE TABLE IF NOT EXISTS security.class (
     class_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    subsystem_id UUID NOT NULL REFERENCES security.subsystem(subsystem_id),
+    subsystem_id UUID NOT NULL
     "name" VARCHAR(50) NOT NULL,
-    UNIQUE (subsystem_id, "name")
+    UNIQUE (subsystem_id, "name"),
+    FOREIGN KEY (subsystem_id) REFERENCES (security.subsystem_id)
 );
 
+--- Table: security.method
 CREATE TABLE IF NOT EXISTS security.method (
     method_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    class_id UUID NOT NULL REFERENCES security.class(class_id),
+    class_id UUID NOT NULL,
     "name" VARCHAR(50) NOT NULL,
-    UNIQUE (class_id, "name")
+    UNIQUE (class_id, "name"),
+    FOREIGN KEY (class_id) REFERENCES (security.class)
 );
 
+--- Table: security.menu
 CREATE TABLE IF NOT EXISTS security.menu (
     menu_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    subsystem_id UUID NOT NULL REFERENCES security.subsystem(subsystem_id),
+    subsystem_id UUID NOT NULL,
     "name" VARCHAR(50) NOT NULL,
-    UNIQUE (subsystem_id, "name")
+    UNIQUE (subsystem_id, "name"),
+    FOREIGN KEY (subsystem_id)
 );
 
+--- Table: security.tx
 CREATE TABLE IF NOT EXISTS security.tx (
     tx_id SERIAL PRIMARY KEY,
     subsystem VARCHAR(50) NOT NULL,
@@ -45,16 +55,19 @@ CREATE TABLE IF NOT EXISTS security.tx (
     UNIQUE (subsystem, class, method)
 );
 
+--- Table: security.user_profile
 CREATE TABLE IF NOT EXISTS security.user_profile (
     user_id UUID NOT NULL REFERENCES security.user(user_id),
     profile_id UUID NOT NULL REFERENCES security.profile(profile_id)
 );
 
+--- Table: security.method_profile
 CREATE TABLE IF NOT EXISTS security.method_profile (
     method_id UUID NOT NULL REFERENCES security.method(method_id),
     profile_id UUID NOT NULL REFERENCES security.profile(profile_id)
 );
 
+--- Table: security.menu_profile
 CREATE TABLE IF NOT EXISTS security.menu_profile (
     menu_id UUID NOT NULL REFERENCES security.menu(menu_id),
     profile_id UUID NOT NULL REFERENCES security.profile(profile_id)
---
+++ backend/migrations/04_people.sql
@@ -0,0 +1,45 @@
+--- Table: people.staff
+CREATE TABLE IF NOT EXISTS people.staff (
+    user_id UUID PRIMARY KEY,
+    phone VARCHAR(20),
+    "address" TEXT,
+    FOREIGN KEY (user_id) REFERENCES security.user(user_id)
+);
+
+--- Table: people.attendee
+CREATE TABLE IF NOT EXISTS people.attendee (
+    attendee_ci INT PRIMARY KEY,
+    user_id UUID UNIQUE NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    surname VARCHAR(100),
+    FOREIGN KEY (user_id) REFERENCES people.attendee(user_id)
+);
+
+--- Table: people.attendee_event
+CREATE TABLE IF NOT EXISTS people.attendee_event (
+    attendee_ci INTEGER NOT NULL,
+    event_id UUID NOT NULL,
+    attended BOOLEAN NOT NULL DEFAULT FALSE,
+    PRIMARY KEY (attendee_ci, event_id),
+    FOREIGN KEY (attendee_ci) REFERENCES people.attendee(attendee_ci),
+    FOREIGN KEY (event_id) REFERENCES events.event(event_id)
+);
+
+--- Table: people.staff_role
+CREATE TABLE IF NOT EXISTS people.staff_role (
+    staff_role_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "name" VARCHAR(100) UNIQUE NOT NULL,
+    desc_txt TEXT
+);
+
+--- Table: people.staff_event
+CREATE TABLE IF NOT EXISTS people.staff_event (
+    staff_id UUID NOT NULL,
+    event_id UUID NOT NULL,
+    staff_role_id INTEGER NOT NULL,
+    cost_amt DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (staff_id, event_id),
+    FOREIGN KEY (staff_id) REFERENCES people.staff(user_id),
+    FOREIGN KEY (event_id) REFERENCES events.event(event_id),
+    FOREIGN KEY (staff_role_id) REFERENCES people.staff_role(staff_role_id)
+);
