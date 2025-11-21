fa11b80768a0fdaf8003c5297f4e7708c3c8a8c5
Rebecca Bracho <rebeccabracho05@gmail.com>
2025-11-21 19:48:39 -0400

    refactor: standardize argument handling in toProcessService methods
    pa que no haya funa

--- frontend/src/services/to-process.js
+++ frontend/src/services/to-process.js
@@ -12,15 +12,26 @@ export const toProcessService = {
     },
 
     // wrappers
-    async getOneUser(tx, args) { return this.toProcess(tx, args); },
-    async createEvent(tx, args) { return this.toProcess(tx, args); },
-    async getAllEvents(tx, args) { return this.toProcess(tx, args); },
-    async getEvent(tx, args) { return this.toProcess(tx, args); },
-    async updateEvent(tx, args) { return this.toProcess(tx, args); },
-    async setEventFlyer(tx, args) { return this.toProcess(tx, args); },
-    async createLocation(tx, args) { return this.toProcess(tx, args); },
-    async getAllLocations(tx, args) { return this.toProcess(tx, args); },
-    async setEventReservation(tx, args) { return this.toProcess(tx, args); },
-    async getEventReservations(tx, args) { return this.toProcess(tx, args); },
-    async getEventAttendances(tx, args) { return this.toProcess(tx, args); }
+
+    // users
+    async getOneUser(args) { return this.toProcess(11, args); },
+    async getManyUsers(args) { return this.toProcess(16, args); },
+
+    // events
+    async createEvent(args) { return this.toProcess(1, args); },
+    async getAllEvents(args) { return this.toProcess(2, args); },
+    async getEvent(args) { return this.toProcess(3, args); },
+    async updateEvent(args) { return this.toProcess(4, args); },
+    async createLocation(args) { return this.toProcess(5, args); },
+    async getAllLocations(args) { return this.toProcess(6, args); },
+    async setEventReservation(args) { return this.toProcess(7, args); },
+    async getEventReservation(args) { return this.toProcess(8, args); },
+    async getEventFlyer(args) { return this.toProcess(10, args); },
+    
+    // attendance
+    async getEventAttendances(args) { return this.toProcess(15, args); },
+    async registerAttendee(args) { return this.toProcess(12, args); },
+    async getUserAttendances(args) { return this.toProcess(13, args); },
+    async checkInAttendee(args) { return this.toProcess(14, args); }
+
 }
